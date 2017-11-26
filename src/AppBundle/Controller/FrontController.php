676842323478f7f835beb55ddcbcbbadd36a1ec0
Arno2T <arnaud.tortora@gmail.com>
2017-11-26 10:00:24 +0100

    Price services

---
+++ src/AppBundle/Controller/FrontController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arno_2T
+ * Date: 20/11/2017
+ * Time: 16:27
+ */
---
+++ src/AppBundle/Services/DefineBookingCode.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arno_2T
+ * Date: 22/11/2017
+ * Time: 16:01
+ */
